cfde441ca9e73643386b1c9c159b27a8e70edaf0
worapong2542 <69861966+worapong2542@users.noreply.github.com>
2020-11-25 13:42:14 +0700

    Update setting.dart

--- lib/menusetting/setting.dart
+++ lib/menusetting/setting.dart
@@ -1,3 +1,4 @@
+import 'package:booku/menusetting/noti.dart';
 import 'package:flutter/material.dart';
 
 import '../mainpage.dart';
@@ -35,9 +36,119 @@ class _SettingState extends State<Setting> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(00),
-      child: new Text('setting'),
-    ));
+      body: Padding(
+        padding: EdgeInsets.all(00),
+        child: ListView(
+          children: <Widget>[
+            backbotton(),
+            SizedBox(
+              height: 15.0,
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      '  Setting',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 1000,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.black,
+                      child: Text('User Account'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Mainpage()), //gotomainpage
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 1000,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.black,
+                      child: Text('Setting'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Noti()), //gotomainpage
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 1000,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.black,
+                      child: Text('Information'),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Setting()), //gotomainpage
+                        );
+                      },
+                    ),
+                  ),
+                  FlatButton(
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                Setting()), //GotoForgotpassword
+                      );
+                    },
+                    textColor: Colors.grey,
+                    child: Text('about?'),
+                  ),
+                  FlatButton(
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                Setting()), //GotoForgotpassword
+                      );
+                    },
+                    textColor: Colors.grey,
+                    child: Text('Help?'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
