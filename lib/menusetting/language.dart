110c82cae0203e7fbfaf4cfb3d881935ec508053
worapong2542 <69861966+worapong2542@users.noreply.github.com>
2020-11-25 13:42:50 +0700

    Update language.dart

--- lib/menusetting/language.dart
+++ lib/menusetting/language.dart
@@ -33,10 +33,133 @@ class _LanguageState extends State<Language> {
 
   @override
   Widget build(BuildContext context) {
+    bool status = false;
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(00),
-      child: new Text('language'),
-    ));
+      body: Padding(
+        padding: EdgeInsets.all(00),
+        child: ListView(
+          children: <Widget>[
+            backbotton(),
+            SizedBox(
+              height: 35.0,
+            ),
+            Container(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Language',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  Text(
+                    'Thai',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 10),
+                  ),
+                  CustomSwitch(
+                    value: status,
+                    onChanged: (value) {
+                      print("VALUE : $value");
+                      setState(() {
+                        status = value;
+                      });
+                    },
+                  ),
+                  Text(
+                    'Eng',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 10),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomSwitch extends StatefulWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  CustomSwitch({Key key, this.value, this.onChanged}) : super(key: key);
+
+  @override
+  _CustomSwitchState createState() => _CustomSwitchState();
+}
+
+class _CustomSwitchState extends State<CustomSwitch>
+    with SingleTickerProviderStateMixin {
+  Animation _circleAnimation;
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 60));
+    _circleAnimation = AlignmentTween(
+            begin: widget.value ? Alignment.centerRight : Alignment.centerLeft,
+            end: widget.value ? Alignment.centerLeft : Alignment.centerRight)
+        .animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.linear));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return GestureDetector(
+          onTap: () {
+            if (_animationController.isCompleted) {
+              _animationController.reverse();
+            } else {
+              _animationController.forward();
+            }
+            widget.value == false
+                ? widget.onChanged(true)
+                : widget.onChanged(false);
+          },
+          child: Container(
+            width: 45.0,
+            height: 28.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24.0),
+              color: _circleAnimation.value == Alignment.centerLeft
+                  ? Colors.grey
+                  : Colors.blue,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  top: 2.0, bottom: 2.0, right: 2.0, left: 2.0),
+              child: Container(
+                alignment:
+                    widget.value ? Alignment.centerRight : Alignment.centerLeft,
+                child: Container(
+                  width: 20.0,
+                  height: 20.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
