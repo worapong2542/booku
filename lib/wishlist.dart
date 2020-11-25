d7a7383ec64390f6343b2673217ede44e04b815f
worapong2542 <69861966+worapong2542@users.noreply.github.com>
2020-11-25 13:38:24 +0700

    Update wishlist.dart

--- lib/wishlist.dart
+++ lib/wishlist.dart
@@ -1,3 +1,5 @@
+import 'package:booku/home.dart';
+import 'package:booku/mainpage.dart';
 import 'package:flutter/material.dart';
 
 class Wishlist extends StatefulWidget {
@@ -6,12 +8,91 @@ class Wishlist extends StatefulWidget {
 }
 
 class _WishlistState extends State<Wishlist> {
+  Widget backbotton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        IconButton(
+          padding: new EdgeInsets.all(0),
+          icon: Icon(Icons.arrow_back_ios, size: 30.0), //insert icon
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Mainpage()), //BacktoScreen
+            );
+          },
+        ),
+        Text(
+          'Back',
+          style: TextStyle(color: Colors.black, fontSize: 13),
+        ),
+      ],
+    );
+  }
+
+  // ignore: non_constant_identifier_names
+  Widget Callbook() {
+    return Container(
+      padding: const EdgeInsets.only(left: 18),
+      height: 142,
+      width: double.infinity,
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: Wrap(
+              children: <Widget>[
+                Image.asset(
+                  "images/book.jpg",
+                  width: 120,
+                  height: 120,
+                ),
+                FlatButton(
+                  padding: EdgeInsets.all(50.0),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  },
+                  textColor: Colors.black,
+                  child: Text(
+                    'booku',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.all(00),
-      child: new Text('wishlist'),
-    ));
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            //1
+            backbotton(),
+            SizedBox(height: 20),
+            Text(
+              "   Wishlist",
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(height: 10),
+            Callbook(),
+            Callbook(),
+            Callbook(),
+            Callbook(),
+          ],
+        ),
+      ),
+    );
   }
 }
