f9219336e861e2efca11c137b8214715b344e34a
Chrbeth <72689032+Chrbeth@users.noreply.github.com>
2020-11-29 01:39:50 +0700

    Add files via upload

--- lib/libary.dart
+++ lib/libary.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:qr_bar_code_flutter/ReadBook.dart';
 
 class Libary extends StatefulWidget {
   Libary({Key key}) : super(key: key);
@@ -10,54 +12,120 @@ class Libary extends StatefulWidget {
 class _LibaryState extends State<Libary> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Container(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 children: <Widget>[
-                  Container(
-                    height: 200,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        Container(
-                          width: 200,
-                          color: Colors.grey,
-                        ),
-                        Container(
-                          width: 200,
-                          color: Colors.blueGrey,
-                        ),
-                        Container(
-                          width: 200,
-                          color: Colors.green,
-                        ),
-                        Container(
-                          width: 200,
-                          color: Colors.grey,
-                        )
-                      ],
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   ),
-                  Container(
-                    height: 500,
-                    child: GridView.count(
-                      crossAxisCount: 3,
-                      children: List.generate(15, (index) {
-                        return Container(
-                          child: Card(
-                            color: Colors.blue,
-                          ),
-                        );
-                      }),
-                    ),
-                  )
+                  SizedBox(height: 10),
+                  BookCard(
+                    title: "Harry Potter",
+                    subtitle: "Part 1",
+                    rating: 4,
+                    imgUrl: 'images/hp1.png',
+                  ),
+                  BookCard(
+                    title: "Harry Potter",
+                    subtitle: "Part 2",
+                    rating: 3,
+                    imgUrl: 'images/hp2.png',
+                  ),
+                  BookCard(
+                    title: "Harry Potter",
+                    subtitle: "Part 3",
+                    rating: 5,
+                    imgUrl: 'images/hp3.png',
+                  ),
                 ],
               ),
             ),
-          ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BookCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final int rating;
+  final String imgUrl;
+  const BookCard({
+    Key key,
+    this.title,
+    this.subtitle,
+    this.rating,
+    this.imgUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return ReadBook();
+        }));
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        height: 170,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 8,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    for (int i = 0; i < rating; i++)
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 25.0,
+                      ),
+                  ],
+                ),
+              ],
+            ),
+            Image.asset(
+              imgUrl,
+              width: 160,
+              fit: BoxFit.fitWidth,
+            ),
+          ],
         ),
       ),
     );
